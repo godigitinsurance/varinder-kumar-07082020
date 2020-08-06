f845c021ea9b52e35f450793a0ce1e1248ee2b1f
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-08-06 11:06:09 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Varinder Kumar");
+$("#policyNumber").text("PDI6208480");
+$("#startDate").text("07-Aug-2020");
+$("#endDate").text("06-Aug-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08CR0280");
+$("#make").text("LML");
+$("#model").text("NV");
+$("#vehicalSubType").text("SPL KICK START");
+$("#engineNo").text("E16E3145022");
+$("#chassis").text("MD711EABKE3616310");
